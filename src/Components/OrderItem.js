c0512c6a648b3afa87900f8e5fb86b7370e4f66c
Mayank <ms772254@gmail.com>
2024-08-01 01:24:20 +0530

    payment integrated and order APIs connected

--- src/Components/OrderItem.js
+++ src/Components/OrderItem.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import classes from './OrderItem.module.css';
 
-const OrderItem = ({ image, size, price, title, onCancel }) => {
+const OrderItem = ({ image, size, price, title, onCancel , orderID , paymentID , quantity }) => {
   return (
     <div className={classes.orderItem}>
       <div className={classes.itemDetails}>
         <img src={image} alt="Product" className={classes.orderItemImage} />
         <div className={classes.detailsText}>
-          <h2>{title}</h2>
+          <h2>{orderID}</h2>
           <h4><span>Price: </span>{price}</h4>
           <h4><span>Size: </span>{size}</h4>
+          <h4><span>Quantity: </span>{quantity}</h4>
+          <h4><span>Payment ID: </span>{paymentID}</h4>
         </div>
         <button className={classes.arrowButton}>&#x2192;</button>
       </div>
--- src/Components/Orderspage.js
+++ src/Components/Orderspage.js
@@ -1,42 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import OrderItem from './OrderItem';
 import classes from './Orderspage.module.css';
-import back from '../images/back.jpg'
+import { getAuthToken } from '../utils/Auth';
 
+function getsize(size){
+  if(size === 'small') return 'S';
+  if(size === 'medium') return 'M';
+  if(size === 'large') return 'L';
+  if(size === 'extraLarge') return 'XL';
+  if(size === 'doubleExtraLarge') return 'XXL';
+}
 const OrdersPage = () => {
+  const [orders,onOrdersChange] = useState([]);
+  useEffect(() => {
+    const orderLoader = async () => {
+      const res = await fetch('http://localhost:8080/orders' , {
+        headers : {
+          'Authorization' : 'bearer ' + getAuthToken()
+        }
+      });
+      if(!res.ok){
+        alert('failed to fetch orders');
+      }
+      const resData = await res.json();
+      console.log(resData);
+      onOrdersChange(resData);
+    }
+    orderLoader();
+  } , [])
   const handleCancel = (orderId) => {
     // Handle cancel order logic here
     console.log(`Cancel order with ID: ${orderId}`);
   };
-
-  const orders = [
-    {
-      id: 1,
-      image: back,
-      title: 'Cotton Wrap Top',
-      size: 'L',
-      price: 'Rs 30',
-    },
-    {
-        id: 1,
-        image: back,
-        title: 'Cotton Wrap Top',
-        size: 'L',
-        price: 'RS 30',
-      },
-    // Add more orders as needed
-  ];
-
   return (
     <div className={classes.ordersPage}>
       <h1>My Orders</h1>
       {orders.map(order => (
         <OrderItem
-          key={order.id}
-          image={order.image}
-          title={order.title}
-          size={order.size}
-          price={order.price}
+          key={order.orderID}
+          orderID={order.orderID}
+          paymentID={order.paymentID}
+          image={order.products[0].image}
+          size={getsize(order.products[0].size)}
+          price={order.products[0].price}
+          quantity={order.products[0].quantity}
           onCancel={() => handleCancel(order.id)}
         />
       ))}
