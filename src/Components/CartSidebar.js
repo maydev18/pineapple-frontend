3058af2da756f9f68e3f9a44936ea13a8363d321
Mayank <ms772254@gmail.com>
2024-08-27 18:18:02 +0530

    fixed issues with cart overlay

--- src/Components/CartSidebar.js
+++ src/Components/CartSidebar.js
@@ -6,40 +6,46 @@ import { Link } from 'react-router-dom';
 import { getsize } from '../utils/cartUtils/convertSize';
 
 const CartSidebar = () => {
-  const {cart , isOpen , closeCart} = useContext(CartContext);
+  const { cart, isOpen, closeCart } = useContext(CartContext);
+
   return (
-    <div className={`${styles.cartSidebar} ${isOpen ? styles.open : ''}`}>
-      <button className={styles.closeButton} onClick={() => {closeCart()}}>X</button>
-      <h1 className={styles.cartHeading}>Cart</h1>
-      <hr />
-      {cart.length === 0 ? (
-        <p>Your cart is empty</p>
-      ) : (
-        <>
-          {cart.map((product, index) => (
-            <CartItem
-              key={index}
-              image={product.productID.mainImage}
-              size={getsize(product.size)}
-              quantity={product.quantity}
-              price={product.productID.price}
-              title={product.productID.title}
-              id={product.productID._id}
-              checkout={false}
-            />
-          ))}
-          <div className={styles.proceedbutton}>
-            <Link to='/checkout'>
-              <button className={styles.checkoutButton} style={{textDecoration: "none"}}>
-                Proceed to Payment
-              </button>
-            </Link>
-          </div>
-        </>
-      )}
-    </div>
+    <>
+      {/* Underlay that appears when the cart is open */}
+      {isOpen && <div className={styles.underlay} onClick={closeCart}></div>}
+      
+      {/* Cart Sidebar */}
+      <div className={`${styles.cartSidebar} ${isOpen ? styles.open : ''}`}>
+        <button className={styles.closeButton} onClick={closeCart}>X</button>
+        <h1 className={styles.cartHeading}>Cart</h1>
+        <hr />
+        {cart.length === 0 ? (
+          <p>Your cart is empty</p>
+        ) : (
+          <>
+            {cart.map((product, index) => (
+              <CartItem
+                key={index}
+                image={product.productID.mainImage}
+                size={getsize(product.size)}
+                quantity={product.quantity}
+                price={product.productID.price}
+                title={product.productID.title}
+                id={product.productID._id}
+                checkout={false}
+              />
+            ))}
+            <div className={styles.proceedbutton}>
+              <Link to='/checkout'>
+                <button className={styles.checkoutButton} style={{ textDecoration: "none" }}>
+                  Proceed to Payment
+                </button>
+              </Link>
+            </div>
+          </>
+        )}
+      </div>
+    </>
   );
 };
-
 
 export default CartSidebar;
