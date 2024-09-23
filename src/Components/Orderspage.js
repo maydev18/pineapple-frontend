5857f8aba8730a11de9cc518305b547a066275ec
Mayank <ms772254@gmail.com>
2024-09-23 19:38:35 +0530

    fixed exchange and delivery issues

--- src/Components/Orderspage.js
+++ src/Components/Orderspage.js
@@ -2,90 +2,11 @@ import React, { useEffect, useState } from 'react';
 import OrderItem from './OrderItem';
 import classes from './Orderspage.module.css';
 import { getAuthToken } from '../utils/Auth';
-import { format } from 'date-fns';
 import { useError } from '../context/ErrorContext';
-import { Title } from '@mui/icons-material';
 const OrdersPage = () => {
   const {showError} = useError();
   const [orders,onOrdersChange] = useState([]);
-  const dummyOrders = [
-    {
-        "_id": "66ec93efc5f2b4aff9204965",
-        "orderID": "order_Oz96po9f2WSo0w",
-        "time": "2024-09-19T21:13:15.214Z",
-        "exchangeProducts": [
-            {
-                "exchangeReason": [
-                    "Size or Fit Issue",
-                    "Wrong Item Delivered"
-                ],
-                "description": "the product was very bad",
-                "product": {
-                    "_id": "66a507acc4720b336848e423",
-                    "price": 1000,
-                    "quantity": 2,
-                    "size": "large",
-                    "image": "http://localhost:8080/1722091436141-DSC04299 copy.jpg",
-                    "title": "coffee brown oversized t-shirt",
-                    "reviewed": true
-                },
-                "_id": "66ec93efc5f2b4aff9204966"
-            },
-            {
-                "exchangeReason": [
-                    "Damaged Product"
-                ],
-                "description": "",
-                "product": {
-                    "_id": "66a50ae1c4720b336848e425",
-                    "price": 1000,
-                    "quantity": 1,
-                    "size": "medium",
-                    "image": "http://localhost:8080/1722092257500-DSC04239 copy.jpg",
-                    "title": "\"better than yesterday\" oversized t-shirt",
-                    "reviewed": false
-                },
-                "_id": "66ec93efc5f2b4aff9204967"
-            }
-        ],
-        "__v": 0
-    },
-    {
-        "_id": "66ec94d92d961c11d702c946",
-        "userID": "66a4f94d41481a1c1f5784ec",
-        "orderID": "order_Oz96po9f2WSo0w",
-        "time": "2024-09-19T21:16:11.046Z",
-        "exchangeProducts": [
-            {
-                "exchangeReason": [
-                    "Size or Fit Issue"
-                ],
-                "description": "",
-                "product": {
-                    "_id": "66a507acc4720b336848e423",
-                    "price": 1000,
-                    "quantity": 2,
-                    "size": "large",
-                    "image": "http://localhost:8080/1722091436141-DSC04299 copy.jpg",
-                    "title": "coffee brown oversized t-shirt",
-                    "reviewed": true
-                },
-                "_id": "66ec94d92d961c11d702c947"
-            }
-        ],
-        "__v": 0
-    }
-];
-
-
-  // Replace orderLoader with setting the dummy data
-  // const orderLoader = () => {
-  //   try {
-  //     onOrdersChange(dummyOrders);
-  //   } catch (err) {
-  //     showError(err.message, 'danger');
-  //   }
-  // };
+  
   async function orderLoader() {
     try {
       const res = await fetch('http://localhost:8080/orders', {
@@ -107,21 +28,21 @@ const OrdersPage = () => {
   useEffect(() => {
     orderLoader();
   } , [])
-  const handleCancel = (orderId) => {
-    console.log(`Cancel order with ID: ${orderId}`);
-  };
   return (
     <div className={classes.ordersPage}>
       <h1>My Orders</h1>
-      {dummyOrders.map((order , index) => (
-        <OrderItem
-          key={index}
-          order={order}
-          onCancel={() => handleCancel(order.id)}
-        />
-      ))}
+      {orders.length === 0 ? (
+        <p>No orders yet</p> 
+      ) : (
+        orders.map((order, index) => (
+          <OrderItem
+            key={index}
+            order={order}
+          />
+        ))
+      )}
     </div>
-  );
+  );  
 };
 
 export default OrdersPage;
--- src/pages/ProductView.js
+++ src/pages/ProductView.js
@@ -7,7 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Accordion } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 import { format } from 'date-fns';
-import { Spinner, Modal, Button } from 'react-bootstrap';
+import { Spinner, Modal } from 'react-bootstrap';
 import { Icon } from '@iconify/react';
 import RatingSummary from '@keyvaluesystems/react-star-rating-summary';
 import VerifiedIcon from '@mui/icons-material/Verified';
@@ -26,8 +26,6 @@ const ProductPage = () => {
   const [selectedSize, setSelectedSize] = useState(sizes[0]);
   const [showAllReviews, setShowAllReviews] = useState(false);
   const [ratingValues, setRatingValues] = useState({ 5: 0, 4: 0, 3: 0, 2: 0, 1: 0 });
-  const [stock, setStock] = useState({ S: 10, M: 8, L: 15, XL: 5, XXL: 0 });
-
   const [showSizeChart, setshowSizeChart] = useState(false);
 
   const handleCloseSizeChart = () => setshowSizeChart(false);
@@ -85,19 +83,18 @@ const ProductPage = () => {
             <p>SELECT A SIZE</p>
             <div className={classes.sizes}>
               {sizes.map((size) => (
-                <><div key={size} className={classes.sizeWrapper}>
+                <div key={size} className={classes.sizeWrapper}>
                   <button
                     className={`${classes.sizeButton} ${size === selectedSize ? classes.selected : ''}`}
                     onClick={() => setSelectedSize(size)}
-                    disabled={stock[size] === 0}
+                    disabled={product[getFullSize(size)] === 0} // Disable button if product is 0
                   >
                     {size}
                   </button>
-                  <p className={classes.stockInfo} style={{ color: "RED", fontSize: '13px', fontWeight: "300" }}>
-                    {stock[size]} left </p>
-
-
-                </div></>
+                  <p className={classes.stockInfo} style={{color: "#fd5c63", fontSize: '15px', fontWeight: "500"}}>
+                    {product[getFullSize(size)] === 0 ? "Out of Stock" : product[getFullSize(size)] + " Left"} 
+                  </p>
+                </div>
               ))}
 
             </div>
