9ff42f7d4d33d8f6becbda621bd92151e402ed2d
maydev18 <90984907+maydev18@users.noreply.github.com>
2024-10-18 16:34:28 +0530

    Update AuthModal.js

--- src/Modal/AuthModal.js
+++ src/Modal/AuthModal.js
@@ -38,10 +38,7 @@ const AuthModal = ({ isOpen, onClose }) => {
       }
       setIsOtpScreen(true);
       seterr("");
-<<<<<<< HEAD
       setOtp("");
-=======
->>>>>>> 8619f496423118105f9e55decbd761c7db495b12
     } catch (err) {
       seterr("Failed to send OTP");
     } finally {
