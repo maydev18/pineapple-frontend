ce807a753c952e9f832d1e7e6c00c0d5f61efc5a
Mayank <ms772254@gmail.com>
2024-12-23 23:32:41 +0530

    updated google auth credentials

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,14 +2,13 @@ import { initializeApp } from "firebase/app";
 import {getAuth} from 'firebase/auth';
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_KEY,
-  authDomain: "pineapple-2c58c.firebaseapp.com",
-  projectId: "pineapple-2c58c",
-  storageBucket: "pineapple-2c58c.appspot.com",
-  messagingSenderId: "747802978349",
-  appId: "1:747802978349:web:17c9ded36375f77c1a30a4",
-  measurementId: "G-9ZH595N75J"
+  authDomain: "pineapple-fd96c.firebaseapp.com",
+  projectId: "pineapple-fd96c",
+  storageBucket: "pineapple-fd96c.firebasestorage.app",
+  messagingSenderId: "944344941305",
+  appId: "1:944344941305:web:9c28ed02bcafb6091ed828",
+  measurementId: "G-EYWYFYNB86"
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
